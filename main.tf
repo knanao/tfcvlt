cce39f7830910497cfffc5bc01ad6a3ac6ca40c4
knanao <nao.7ken@gmail.com>
2023-01-10 21:10:32 +0900

    Add vault storage resources (#4)

--- main.tf
+++ main.tf
@@ -19,3 +19,33 @@ resource "google_storage_bucket_iam_member" "vault-data" {
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${google_service_account.vault-server.email}"
 }
+
+resource "google_secret_manager_secret" "vault-server-config" {
+  secret_id = "vault-server-config"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "vault-server-config" {
+  secret = google_secret_manager_secret.vault-server-config.id
+
+  secret_data = file("./vault-server.hcl")
+}
+
+resource "google_secret_manager_secret_iam_member" "vault-server-config" {
+  secret_id = google_secret_manager_secret.vault-server-config.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.vault-server.email}"
+}
+
+resource "google_kms_key_ring" "vault-server" {
+  name     = "vault-server"
+  location = "global"
+}
+
+resource "google_kms_crypto_key" "vault-seal" {
+  name     = "vault-seal"
+  key_ring = google_kms_key_ring.vault-server.id
+}
