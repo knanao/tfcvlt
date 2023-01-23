0d657d101ba6c92cba42c1fab6beaf88b011d61d
knanao <nao.7ken@gmail.com>
2023-01-23 15:14:44 +0900

    Add a gcs resource for the example (#25)

--- infra/dev/provider.tf
+++ infra/dev/provider.tf
@@ -1,10 +1,11 @@
+# Enable this provider after configuring a gcp secret engine.
 # provider "google" {
 #   project = var.gcp_project
 #   region  = var.gcp_region
 #   # credentials = base64decode(data.vault_generic_secret.terraform.data["private_key_data"])
 # 
 #   # This should be used if need to wait the desired period until being enabled SA.
-#   # credentials = data.external.credentials.result["credentials"]
+#   credentials = data.external.credentials.result["credentials"]
 # }
 
 # data "vault_generic_secret" "terraform" {
