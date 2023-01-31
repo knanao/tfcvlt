3d13f36476e16c3d28949cdecc68bd384f545e4a
knanao <nao.7ken@gmail.com>
2023-01-31 15:01:15 +0900

    Add a GCS storage (#38)

--- infra/dev/provider.tf
+++ infra/dev/provider.tf
@@ -1,24 +1,24 @@
 # Enable this provider after configuring a gcp secret engine.
-#provider "google" {
-#  project = var.gcp_project
-#  region  = var.gcp_region
-#  # credentials = base64decode(data.vault_generic_secret.terraform.data["private_key_data"])
-#
-#  # This should be used if need to wait the desired period until being enabled SA.
-#  credentials = data.external.credentials.result["credentials"]
-#}
-#
-#data "vault_generic_secret" "terraform" {
-#  path = "gcp/key/terraform"
-#}
-#
-#data "external" "credentials" {
-#  program = ["./gcp-credentials.sh"]
-#
-#  query = {
-#    credentials = base64decode(data.vault_generic_secret.terraform.data["private_key_data"])
-#  }
-#}
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+  # credentials = base64decode(data.vault_generic_secret.terraform.data["private_key_data"])
+
+  # This should be used if need to wait the desired period until being enabled SA.
+  credentials = data.external.credentials.result["credentials"]
+}
+
+data "vault_generic_secret" "terraform" {
+  path = "gcp/key/terraform"
+}
+
+data "external" "credentials" {
+  program = ["./gcp-credentials.sh"]
+
+  query = {
+    credentials = base64decode(data.vault_generic_secret.terraform.data["private_key_data"])
+  }
+}
 
 provider "vault" {
   auth_login {
