1ffe90d45d0c3919f00c0bf33916ca92a3cad60b
knanao <nao.7ken@gmail.com>
2023-01-31 09:41:28 +0900

    Add a test storage

--- infra/dev/storage.tf
+++ infra/dev/storage.tf
@@ -1,5 +1,5 @@
 # Please check whether enabling google provider.
-# resource "google_storage_bucket" "test" {
-#   name     = "${var.gcp_project}-test"
-#   location = var.gcp_region
-# }
+resource "google_storage_bucket" "test" {
+  name     = "${var.gcp_project}-test"
+  location = var.gcp_region
+}
